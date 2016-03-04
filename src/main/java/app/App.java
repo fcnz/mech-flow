850b6db3c382bc622b202b11fa9ea17df914f7fb
fcnz <fraser.croad@gmail.com>
2016-03-05 10:46:52 +1300

    Added User class

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,11 +2,8 @@ package app;
 
 import org.apache.log4j.Logger;
 
-import app.controllers.Controllable;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -51,12 +48,6 @@ public class App extends Application {
 
 		Pane rootLayout = _router.load("RootLayout", this);
 
-//		FXMLLoader loader = new FXMLLoader();
-//		loader.setLocation(this.getClass().getClassLoader().getResource("app/templates/RootLayout.fxml"));
-//		BorderPane rootLayout = (BorderPane) loader.load();
-//		Controllable controller = loader.getController();
-//		controller.setApp(this);
-//
 		Scene scene = new Scene(rootLayout);
 		_primaryStage.setScene(scene);
 		_primaryStage.show();
---
+++ src/main/java/app/models/User.java
@@ -0,0 +1,13 @@
+package app.models;
+
+public class User {
+
+	private final String _USERNAME;
+
+	private String _fullName;
+
+	public User(String username) {
+		_USERNAME = username;
+	}
+
+}
